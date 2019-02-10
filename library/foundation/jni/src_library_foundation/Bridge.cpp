cf37ed9508ffe048700cedcc4a2c688c40b623d9
wzsayiie <wzsayiie@qq.com>
2019-02-10 22:57:46 +0800

    correct a mistake

--- library/foundation/jni/src_library_foundation/Bridge.cpp
+++ library/foundation/jni/src_library_foundation/Bridge.cpp
@@ -1,9 +1,9 @@
 #include "cqbridge.hh"
 
-CQ_EXPORT void CQ_Logger_info(CQType message) {
+CQ_C_LINK void CQ_Logger_info(CQType message) {
     CQJavaCallStatic("src/library/foundation/Bridge", "info", message, CQTypeNULL, CQTypeNULL, CQTypeNULL);
 }
 
-CQ_EXPORT void CQ_Logger_error(CQType message) {
+CQ_C_LINK void CQ_Logger_error(CQType message) {
     CQJavaCallStatic("src/library/foundation/Bridge", "error", message, CQTypeNULL, CQTypeNULL, CQTypeNULL);
 }
