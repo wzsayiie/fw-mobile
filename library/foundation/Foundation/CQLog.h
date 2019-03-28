9018c25ea9b17b4daf11857707a94109c29869a7
wujia <wujia@nibirutech.com>
2019-03-28 14:41:31 +0800

    add ios armv7, armv7s archives

--- library/foundation/Foundation/CQLog.h
+++ library/foundation/Foundation/CQLog.h
@@ -1,13 +1,8 @@
 #import <Foundation/Foundation.h>
+#import "cqconvention.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-void CQLogInfo (NSString *file, int line, NSString *format, ...) NS_FORMAT_FUNCTION(3, 4);
-void CQLogError(NSString *file, int line, NSString *format, ...) NS_FORMAT_FUNCTION(3, 4);
-#ifdef __cplusplus
-}
-#endif
+CQ_C_LINK void CQLogInfo (NSString *file, int line, NSString *format, ...) NS_FORMAT_FUNCTION(3, 4);
+CQ_C_LINK void CQLogError(NSString *file, int line, NSString *format, ...) NS_FORMAT_FUNCTION(3, 4);
 
 #define I(...) CQLogInfo (@(__FILE__), __LINE__, __VA_ARGS__)
 #define E(...) CQLogError(@(__FILE__), __LINE__, __VA_ARGS__)
