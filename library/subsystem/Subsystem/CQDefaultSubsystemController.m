3c7dda8e97c72599775bd8c0cbbb2769b5332cc9
wujia <wujia@nibirutech.com>
2019-04-22 14:31:52 +0800

    ios library assets support

--- library/subsystem/Subsystem/CQDefaultSubsystemController.m
+++ library/subsystem/Subsystem/CQDefaultSubsystemController.m
@@ -5,9 +5,22 @@
 
 @implementation CQDefaultSubsystemController
 
+- (instancetype)init {
+    return [super initWithNibName:@"CQDefaultSubsystemController" bundle:self.assetsBundle];
+}
+
+- (NSBundle *)assetsBundle {
+    NSString *bundlePath = [NSBundle.mainBundle pathForResource:@"Assets" ofType:@"bundle"];
+    NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+    return bundle;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+- (IBAction)handleCloseButtonTouch:(UIButton *)sender {
+    [self removeFromUI];
 }
 
 @end
