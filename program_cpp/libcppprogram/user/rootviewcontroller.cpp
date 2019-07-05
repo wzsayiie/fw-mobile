216124648900ab4942db45c6cdb37b2e04f4b7a6
wujia <wujia@nibirutech.com>
2019-07-05 09:52:20 +0800

    code style

--- program_cpp/libcppprogram/user/rootviewcontroller.cpp
+++ program_cpp/libcppprogram/user/rootviewcontroller.cpp
@@ -12,17 +12,17 @@ void RootViewController::viewDidLoad() {
     super::viewDidLoad();
 }
 
-void RootViewController::touchesBegan
-/**/(const std::set<cqTouchRef> &touches, cqEventRef event)
+void RootViewController::touchesBegan(
+    const std::set<cqTouchRef> &touches, cqEventRef event)
 {
 }
 
-void RootViewController::touchesMoved
-/**/(const std::set<cqTouchRef> &touches, cqEventRef event)
+void RootViewController::touchesMoved(
+    const std::set<cqTouchRef> &touches, cqEventRef event)
 {
 }
 
-void RootViewController::touchesEnded
-/**/(const std::set<cqTouchRef> &touches, cqEventRef event)
+void RootViewController::touchesEnded(
+    const std::set<cqTouchRef> &touches, cqEventRef event)
 {
 }
