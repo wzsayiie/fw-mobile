6ab6cec19471dcaef09b6c222dc830b72b29d4e1
wujia <wujia@nibirutech.com>
2019-07-09 22:26:29 +0800

    lib windows build script

--- generate_headers.bat
+++ generate_headers.bat
@@ -1,1 +1,45 @@
-::unimplement
+@echo off
+
+cd /d %~dp0
+
+::This batch will traverse headers and create new homonymic headers reference former.
+::It will avoid developer adding vast header search paths tediously.
+
+set destination=GENERATED_HEADERS
+
+if exist %destination% (rmdir /q /s %destination%)
+mkdir %destination%
+
+set error=0
+
+set source="library"
+call :generate
+if not %error% == 0 (goto end)
+
+set source="program_basis"
+call :generate
+if not %error% == 0 (goto end)
+
+set source="program_cpp"
+call :generate
+if not %error% == 0 (goto end)
+
+set source="program_lua"
+call :generate
+if not %error% == 0 (goto end)
+
+goto end
+
+:generate
+for /r %source% %%f in (*.h*) do (
+    if exist %~dp0\%destination%\%%~nxf (
+        echo error: homonymic header '%%~nxf'
+        rmdir /q /s %destination%
+        set error=1
+        goto end
+    ) else (
+        echo #include "%%f" >> %~dp0\%destination%\%%~nxf
+    )
+)
+
+:end
--- makelib_windows.bat
+++ makelib_windows.bat
@@ -1,1 +1,65 @@
-::unimplemented
+@echo off
+
+cd /d %~dp0
+
+::generate headers
+call generate_headers.bat
+
+::tmp directory
+set temporary=TMP
+
+if exist %temporary% (rmdir /q /s %temporary%)
+mkdir %temporary%
+
+::compile
+set error=0
+
+set source="library/BASIS/libbasis"
+call :compile_std
+if not %error% == 0 (goto end)
+
+set source="library/ctool/libctool"
+call :compile_std
+if not %error% == 0 (goto end)
+
+set source="library/foundation/libfoundation"
+call :compile_std
+if not %error% == 0 (goto end)
+
+set source="library/foundation/WINFoundation"
+call :compile_cli
+if not %error% == 0 (goto end)
+
+set source="library/standalone/libstandalone"
+call :compile_std
+if not %error% == 0 (goto end)
+
+::link
+set out=BUILD
+set exe=windows
+
+if not exist %out% (mkdir %out%)
+if exist %out%\%exe% (del %out%\%exe%)
+
+cl /nologo %temporary%\*.obj /Fe%out%\%exe%
+
+rmdir /q /s %temporary%
+
+::functions
+goto end
+
+:compile_std
+set args=/nologo /EHsc /std:c++14 /IGENERATED_HEADERS
+for /r %source% %%f in (*.cpp) do (
+    cl %args% /c %%f /Fo%temporary%\%%~nf
+)
+goto end
+
+:compile_cli
+set args=/nologo /clr /std:c++14 /IGENERATED_HEADERS
+for /r %source% %%f in (*.cpp) do (
+    cl %args% /c %%f /Fo%temporary%\%%~nf
+)
+goto end
+
+:end
