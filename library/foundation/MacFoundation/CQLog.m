634b39637c96435c356af8544322aa0e4ec0ba53
wujia <wujia@nibirutech.com>
2019-07-17 13:05:06 +0800

    ios utf8 log

--- library/foundation/MacFoundation/CQLog.m
+++ library/foundation/MacFoundation/CQLog.m
@@ -3,24 +3,32 @@
 
 static void logging(NSString *tag, NSString *file, int line, NSString *message) {
     if (file.length > 0 && line > 0) {
-        NSLog(@"%@|%@(%04d)|%@\n", tag, file, line, message);
+        NSLog(@"%@|%@(%04d)|%@", tag, file, line, message);
     } else {
-        NSLog(@"%@|%@\n", tag, message);
+        NSLog(@"%@|%@", tag, message);
     }
 }
 
 void CQLogInfo(NSString *file, int line, NSString *format, ...) {
-    va_list args;
-    va_start(args, format);
-    NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
-    va_end(args);
+    //[NSString initWithFormat:arguments:] depends on local text encoding,
+    //use vsnprintf() to splice utf-8 string.
+    char buffer[1024]; {
+        va_list args;
+        va_start(args, format);
+        vsnprintf(buffer, sizeof(buffer), format.UTF8String, args);
+        va_end(args);
+    }
+    NSString *message = [NSString stringWithUTF8String:buffer];
     logging(@"I", file.lastPathComponent, line, message);
 }
 
 void CQLogError(NSString *file, int line, NSString *format, ...) {
-    va_list args;
-    va_start(args, format);
-    NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
-    va_end(args);
+    char buffer[1024]; {
+        va_list args;
+        va_start(args, format);
+        vsnprintf(buffer, sizeof(buffer), format.UTF8String, args);
+        va_end(args);
+    }
+    NSString *message = [NSString stringWithUTF8String:buffer];
     logging(@"E", file.lastPathComponent, line, message);
 }
