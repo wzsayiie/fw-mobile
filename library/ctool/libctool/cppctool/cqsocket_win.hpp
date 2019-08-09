aac64c9a047416246fe07f775866ebd372e69fde
wujia <wujia@nibirutech.com>
2019-08-09 22:20:20 +0800

    win sock

--- library/ctool/libctool/cppctool/cqsocket_win.hpp
+++ library/ctool/libctool/cppctool/cqsocket_win.hpp
@@ -1,6 +1,8 @@
 ﻿//
 
-in_addr inet_addr_n(const char *str) {
+_CQCTOOL_BEGIN_VERSION_NS
+
+in_addr _inet_addr(const char *str) {
     in_addr addr;
     memset(&addr, 0, sizeof(addr));
     
@@ -10,7 +12,7 @@ in_addr inet_addr_n(const char *str) {
     return addr;
 }
 
-in6_addr inet6_addr_n(const char *str) {
+in6_addr _inet6_addr(const char *str) {
     in6_addr addr;
     memset(&addr, 0, sizeof(addr));
     
@@ -20,68 +22,126 @@ in6_addr inet6_addr_n(const char *str) {
     return addr;
 }
 
-in_addr inet_addr_n(const std::string &str) {
-    return inet_addr_n(str.c_str());
+in_addr _inet_addr(const std::string &str) {
+    return _inet_addr(str.c_str());
 }
 
-in6_addr inet6_addr_n(const std::string &str) {
-    return inet6_addr_n(str.c_str());
+in6_addr _inet6_addr(const std::string &str) {
+    return _inet6_addr(str.c_str());
 }
 
-std::string inet_str_n(in_addr addr) {
-    char str[INET_ADDRSTRLEN];
+std::string _inet_str(in_addr addr) {
+    char str[INET_ADDRSTRLEN] = {0};
     InetNtopA(AF_INET, &addr, str, sizeof(str));
     return str;
 }
 
-std::string inet6_str_n(in6_addr addr) {
-    char str[INET6_ADDRSTRLEN];
+std::string _inet6_str(in6_addr addr) {
+    char str[INET6_ADDRSTRLEN] = {0};
     InetNtopA(AF_INET6, &addr, str, sizeof(str));
     return str;
 }
 
-SOCKET socket_n(int af, int sock, int ipproto) {
-    static bool initialized = false;
-    if (!initialized) {
-        WSAData data;
-        WSAStartup(MAKEWORD(2, 2), &data);
-        initialized = true;
+static _socket_t pkg(SOCKET so) {return (_socket_t)so;}
+static SOCKET raw(_socket_t so) {return (SOCKET)so;}
+
+static _socket_t _socket(int af, int sock, int ipproto) {
+    static bool init = false;
+    if (!init) {
+        WSAData dat;
+        WSAStartup(MAKEWORD(2, 2), &dat);
+        init = true;
     }
 
-    return socket(af, sock, ipproto);
+    SOCKET so = socket(af, sock, ipproto);
+    if (so != INVALID_SOCKET) {
+        return pkg(so);
+    } else {
+        return nullptr;
+    }
 }
 
-void close_n(SOCKET s) {
-    closesocket(s);
+_socket_t _tcp_sock () { return _socket(AF_INET , SOCK_STREAM, IPPROTO_TCP); }
+_socket_t _tcp_sock6() { return _socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP); }
+_socket_t _udp_sock () { return _socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP); }
+_socket_t _udp_sock6() { return _socket(AF_INET6, SOCK_DGRAM , IPPROTO_UDP); }
+
+void _close(_socket_t so) {
+    closesocket(raw(so));
 }
 
-const char *_last_socket_error_n() {
-    std::string ret;
-    
-    int nCode = WSAGetLastError();
-    if (nCode != 0) {
+bool _bind(_socket_t localso, _sockaddr localaddr) {
+    int code = bind(raw(localso), localaddr.raw(), localaddr.len());
+    return code == 0;
+}
 
-        ret.append("WSAGetLastError:");
-        ret.append(std::to_string(nCode));
+bool _listen(_socket_t localso) {
+    int code = listen(raw(localso), SOMAXCONN);
+    return code == 0;
+}
+
+_socket_t _accept(_socket_t localso, _sockaddr *endaddr) {
+    sockaddr *addr = endaddr ? endaddr->raw() : nullptr;
+    int    addrlen = endaddr ? endaddr->len() : 0;
+
+    SOCKET endso = accept(raw(localso), addr, &addrlen);
+    return pkg(endso);
+}
+
+bool _connect(_socket_t localso, _sockaddr endaddr) {
+    int code = connect(raw(localso), endaddr.raw(), endaddr.len());
+    return code == 0;
+}
+
+int _send(_socket_t endso, const void *dat, int datlen) {
+    return send(raw(endso), (const char *)dat, datlen, 0);
+}
+
+int _recv(_socket_t endso, void *buf, int buflen) {
+    return recv(raw(endso), (char *)buf, buflen, 0);
+}
+
+int _sendto(_socket_t localso, _sockaddr endaddr, const void *dat, int datlen) {
+    return sendto(raw(localso), (const char *)dat, datlen, 0, endaddr.raw(), endaddr.len());
+}
+
+int _recvfrom(_socket_t localso, _sockaddr *endaddr, void *buf, int buflen) {
+    sockaddr *addr = endaddr ? endaddr->raw() : nullptr;
+    int    addrlen = endaddr ? endaddr->len() : 0;
+
+    return recvfrom(raw(localso), (char *)buf, buflen, 0, addr, &addrlen);
+}
+
+const char *_error(_socket_t) {
+
+    int code = WSAGetLastError();
+    if (code == 0) {
+        return cq_store_str(nullptr);
+    }
+
+    std::string msg;
+    msg.append("WSAGetLastError:");
+    msg.append(std::to_string(code));
         
-        //NOTE: in english to avoid multi-byte characters.
-        DWORD dwLanguageId = MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT);
-        LPSTR pszMessage = NULL;
-        FormatMessageA(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-            NULL,
-            nCode,
-            dwLanguageId,
-            (LPSTR)&pszMessage,
-            0,
-            NULL);
+    //NOTE: in english to avoid multi-byte characters.
+    DWORD lang = MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT);
+    LPSTR text = NULL;
+    FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL,
+        code,
+        lang,
+        (LPSTR)&text,
+        0,
+        NULL);
 
-        if (!cq_str_empty(pszMessage)) {
-            ret.append(", ");
-            ret.append(pszMessage);
-            LocalFree(pszMessage);
-        }
+    if (!cq_str_empty(text)) {
+        msg.append(", ");
+        msg.append(text);
+        LocalFree(text);
     }
-    
-    return cq_store_str(ret.c_str());
+
+    return cq_store_str(msg.c_str());
 }
+
+_CQCTOOL_END_VERSION_NS
