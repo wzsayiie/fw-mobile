b1a762ef5916e5c33be384b36327ebba01bc4c71
wujia <wujia@nibirutech.com>
2019-08-20 11:15:22 +0800

    copy apk bundle assert to disk

--- library/foundation/android/java/src/library/foundation/FileAssist.java
+++ library/foundation/android/java/src/library/foundation/FileAssist.java
@@ -3,6 +3,10 @@ package src.library.foundation;
 import android.app.Application;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import src.library.basis.StringUtil;
 
@@ -76,6 +80,43 @@ public class FileAssist {
         if (StringUtil.isEmpty(path)) {
             File file = new File(path);
             removeRecursively(file);
+        }
+    }
+
+    public static void copyAssetToDir(String assetName, String dir, String fileName) {
+        if (StringUtil.isEmpty(assetName)) {
+            return;
+        }
+        if (StringUtil.isEmpty(dir)) {
+            return;
+        }
+        if (StringUtil.isEmpty(fileName)) {
+            return;
+        }
+
+        File outFile = new File(dir, fileName);
+        if (outFile.exists()) {
+            removeRecursively(outFile);
+        }
+
+        try {
+            Application app = AppContextAssist.getApp();
+            if (app == null) {
+                return;
+            }
+            InputStream inStream = app.getAssets().open(assetName);
+            OutputStream outStream = new FileOutputStream(outFile);
+
+            byte[] buffer = new byte[4 * 1024];
+            int count;
+            while ((count = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, count);
+            }
+
+            inStream.close();
+            outStream.close();
+
+        } catch (IOException ignored) {
         }
     }
 }
