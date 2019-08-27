7cb265f74c2ba662bb4f7a17d1d8c56840436513
wujia <wujia@nibirutech.com>
2019-08-27 15:04:32 +0800

    unity AndroidBuilder

--- app_unity/Assets/Editor/Builder/AndroidBuilder.cs
+++ app_unity/Assets/Editor/Builder/AndroidBuilder.cs
@@ -1,28 +1,27 @@
+using System;
 using System.IO;
 using UnityEditor;
 
-class AndroidBuilder
+public static class AndroidBuilder
 {
-    // configuration begin
-    const string AppPackageID = "domain.corp.store.flavor.name";
+    ///configuration begin
+    private const string AppPackageID = "domain.corp.store.flavor.name";
 
-    const string KeystoreFile = "MobileKeystore/Android/master.jks";
-    const string KeystorePass = "master";
-    const string KeyaliasName = "master";
-    const string KeyaliasPass = "master";
+    private const string KeystoreFile = "MobileKeystore/Android/master.jks";
+    private const string KeystorePass = "master";
+    private const string KeyaliasName = "master";
+    private const string KeyaliasPass = "master";
 
-    const string BuiltAPKName = "unity.apk";
+    private const string ExportAKPDir = "../BUILD";
+    private const string BuiltAPKName = "unity.apk";
 
-    static string[] Scenes
-    {
-        get
-        {
-            return new string[] { "Assets/Scenes/LaunchScene.unity" };
-        }
-    }
-    // configuration end
+    private static readonly string[] Scenes = {
+        "Assets/Scene/LaunchScene.unity",
+        "Assets/Scene/WorldScene.unity"
+    };
+    ///configuration end
 
-    static void Build()
+    public static void Build()
     {
         if (File.Exists(BuiltAPKName))
         {
@@ -35,16 +34,34 @@ class AndroidBuilder
         PlayerSettings.Android.keyaliasPass = KeyaliasPass;
         BuildPipeline.BuildPlayer(Scenes, BuiltAPKName, BuildTarget.Android, BuildOptions.None);
 
-        string toDir = "../BUILD";
-        string toAPK = "../BUILD/" + BuiltAPKName;
-        if (!Directory.Exists(toDir))
+        if (!Directory.Exists(ExportAKPDir))
         {
-            Directory.CreateDirectory(toDir);
+            Directory.CreateDirectory(ExportAKPDir);
         }
+
+        //move apk.
+        string toAPK = ExportAKPDir + "/" + BuiltAPKName;
         if (File.Exists(toAPK))
         {
             File.Delete(toAPK);
         }
         File.Move(BuiltAPKName, toAPK);
+
+        //move symbols.zip of il2cpp.
+        string symbolsSuffix = ".symbols.zip";
+        foreach (string it in Directory.GetFiles(ExportAKPDir))
+        {
+            if (it.EndsWith(symbolsSuffix, StringComparison.OrdinalIgnoreCase))
+            {
+                File.Delete(it);
+            }
+        }
+        foreach (string it in Directory.GetFiles("."))
+        {
+            if (it.EndsWith(symbolsSuffix, StringComparison.OrdinalIgnoreCase))
+            {
+                File.Move(it, ExportAKPDir + "/" + Path.GetFileName(it));
+            }
+        }
     }
 }
