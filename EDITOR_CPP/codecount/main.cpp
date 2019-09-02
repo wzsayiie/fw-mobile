f98c3fd748a25d01b9d29f1b73a05f7571d3251a
wujia <wujia@nibirutech.com>
2019-09-02 17:50:05 +0800

    codecount

--- EDITOR_CPP/codecount/main.cpp
+++ EDITOR_CPP/codecount/main.cpp
@@ -16,8 +16,8 @@ const vector<string> &dirs(bool push, const char *item) {
 }
 
 void app_android() { dirs(_JAVA, "app_android/app/src/main/java"); }
-void app_ios    () { dirs(_OBJC, "app_ios/App/Sources"          ); }
-void app_unity  () { dirs(_CS  , "app_unity/Assets/Scripts"     ); }
+void app_ios    () { dirs(_OBJC, "app_ios/App/Classes"          ); }
+void app_unity  () { dirs(_CS  , "app_unity/Assets/Script"      ); }
 
 void EDITOR_CPP() { dirs(_CPP, "EDITOR_CPP"); }
 void EDITOR_CS () { dirs(_CS , "EDITOR_CS" ); }
@@ -55,26 +55,28 @@ void library_subsystem() {
     dirs(_CPP , "library/subsystem/libsubsystem");
 }
 
-void program_basis() {
-    dirs(_JNI , "program_basis/android/cpp"    );
-    dirs(_JAVA, "program_basis/android/java"   );
-    dirs(_OBJC, "program_basis/IOSBasisProgram");
-    dirs(_CPP , "program_basis/libbasisprogram");
+void program_app() {
+    dirs(_JNI , "program_app/appkit/android/cpp" );
+    dirs(_JAVA, "program_app/appkit/android/java");
+    dirs(_OBJC, "program_app/appkit/IOSAppKit"   );
+    dirs(_CPP , "program_app/appkit/libappkit"   );
+    dirs(_CPP , "program_app/userapp"            );
 }
 
-void program_cpp() {
-    dirs(_JNI , "program_cpp/android/cpp"  );
-    dirs(_JAVA, "program_cpp/android/java" );
-    dirs(_OBJC, "program_cpp/IOSCPPProgram");
-    dirs(_CPP , "program_cpp/libcppprogram");
+void program_game() {
+    dirs(_JNI , "program_game/gamekit/android/cpp" );
+    dirs(_JAVA, "program_game/gamekit/android/java");
+    dirs(_OBJC, "program_game/gamekit/IOSGameKit"  );
+    dirs(_CPP , "program_game/gamekit/libgamekit"  );
+    dirs(_CPP , "program_game/usergame"            );
+    dirs(_LUA , "program_game/userscript"          );
 }
 
-void program_lua() {
-    dirs(_JNI , "program_lua/android/cpp"  );
-    dirs(_JAVA, "program_lua/android/java" );
-    dirs(_OBJC, "program_lua/IOSLuaProgram");
-    dirs(_CPP , "program_lua/libluaprogram");
-    dirs(_LUA , "program_lua/libluascript" );
+void program_gl() {
+    dirs(_JNI , "program_gl/glkit/android/cpp"  );
+    dirs(_JAVA, "program_gl/glkit/android/java" );
+    dirs(_OBJC, "program_gl/glkit/IOSGLKit"     );
+    dirs(_CPP , "program_gl/glkit/libglkit"     );
 }
 
 bool codecount_launch = 0;
@@ -100,9 +102,9 @@ void codecount_main() {
     library_standalone();
     library_subsystem();
     
-    program_basis();
-    program_cpp();
-    program_lua();
+    program_app();
+    program_game();
+    program_gl();
     
     codecount(dirs(false, nullptr));
 }
