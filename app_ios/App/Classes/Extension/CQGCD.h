36c204f8a02b32f56571b1611a7d70db5aed4311
wzsayiie <wzsayiie@qq.com>
2019-09-03 22:23:25 +0800

    dispatch async delay

--- app_ios/App/Classes/Extension/CQGCD.h
+++ app_ios/App/Classes/Extension/CQGCD.h
@@ -1,6 +1,6 @@
 //
 
-//post $block to dispatch_get_global_queue.
-void _dispatch_async(dispatch_block_t block);
+//post $block to dispatch_get_global_queue().
+void _dispatch_async(float delayInSeconds, dispatch_block_t block);
 
-void _dispatch_async_on_main(dispatch_block_t block);
+void _dispatch_async_on_main(float delayInSeconds, dispatch_block_t block);
--- app_ios/App/Classes/Extension/CQGCD.m
+++ app_ios/App/Classes/Extension/CQGCD.m
@@ -1,9 +1,26 @@
 #import "CQGCD.h"
 
-void _dispatch_async(dispatch_block_t block) {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block);
+static void async(float delayInSeconds, dispatch_queue_t queue, dispatch_block_t block) {
+    if (block == NULL) {
+        return;
+    }
+    
+    if (delayInSeconds > 0) {
+        dispatch_time_t time = dispatch_time(
+            DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC)
+        );
+        dispatch_after(time, queue, block);
+    } else {
+        dispatch_async(queue, block);
+    }
 }
 
-void _dispatch_async_on_main(dispatch_block_t block) {
-    dispatch_async(dispatch_get_main_queue(), block);
+void _dispatch_async(float delayInSeconds, dispatch_block_t block) {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    async(delayInSeconds, queue, block);
+}
+
+void _dispatch_async_on_main(float delayInSeconds, dispatch_block_t block) {
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    async(delayInSeconds, queue, block);
 }
