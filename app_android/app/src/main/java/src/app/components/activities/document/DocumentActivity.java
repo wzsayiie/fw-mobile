951cbbcf8b67187cdafec0f5c78229fddfb58704
wujia <wujia@nibirutech.com>
2019-09-06 22:19:15 +0800

    DocumentActivity

---
+++ app_android/app/src/main/java/src/app/components/activities/document/DocumentActivity.java
@@ -0,0 +1,15 @@
+package src.app.components.activities.document;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import src.library.basis.W;
+
+@SuppressWarnings({W.APP_OMIT_0, W.APP_OMIT_1, W.APP_OMIT_2, W.APP_OMIT_3})
+public class DocumentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
