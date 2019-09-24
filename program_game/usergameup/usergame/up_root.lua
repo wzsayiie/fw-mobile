114aff33be7329415f493cb370121a2b3a50e191
wujia <wujia@nibirutech.com>
2019-09-24 19:35:53 +0800

    up_root

--- program_game/usergameup/usergame/up_root.lua
+++ program_game/usergameup/usergame/up_root.lua
@@ -6,13 +6,7 @@ up_root = class("up_root", code_beh, {
 function up_root:awake()
 end
 
-function up_root:start()
-end
-
 function up_root:update()
-end
-
-function up_root:on_destroy()
 end
 
 --)R"
