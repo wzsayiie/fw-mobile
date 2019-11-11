9d36f51b123cf8944de9197d5da3cc7f491c3aa0
wzsayiie <wzsayiie@qq.com>
2019-11-11 15:02:43 +0800

    modify build scripts because change of unity build method names.

--- build_unity_android.bat
+++ build_unity_android.bat
@@ -8,7 +8,7 @@ set cmd_line=%cmd_line% -quit
 set cmd_line=%cmd_line% -batchmode
 set cmd_line=%cmd_line% -buildTarget Android
 set cmd_line=%cmd_line% -projectPath %cd%\app_unity
-set cmd_line=%cmd_line% -executeMethod AndroidBuilder.Build
+set cmd_line=%cmd_line% -executeMethod AndroidBuild.Build
 set cmd_line=%cmd_line% -logFile
 
 %cmd_line%
--- export_unity_pkg.bat
+++ export_unity_pkg.bat
@@ -7,7 +7,7 @@ set cmd_line=%cmd_line% Unity.exe
 set cmd_line=%cmd_line% -quit
 set cmd_line=%cmd_line% -batchmode
 set cmd_line=%cmd_line% -projectPath %cd%\app_unity
-set cmd_line=%cmd_line% -executeMethod SDKExporter.Export
+set cmd_line=%cmd_line% -executeMethod SDKExport.Export
 set cmd_line=%cmd_line% -logFile
 
 %cmd_line%
