2a39f904508d2af4d73aaefa2d6896771c731a8f
wzsayiie <wzsayiie@qq.com>
2019-11-12 17:30:27 +0800

    gamekit, add class spriterenderer.

--- program_game/gamekit/libgamekit/cppgamekit/csspriterenderder.cpp
+++ program_game/gamekit/libgamekit/cppgamekit/csspriterenderder.cpp
@@ -1,7 +1,37 @@
 #include "csspriterenderder.hh"
+#include "cqglkit.h"
 
 cq_member(csSpriteRenderer) {
+    csSpriteRef sprite;
+    csVector2 size;
 };
 
 void csSpriteRenderer::handleRender() {
+}
+
+void csSpriteRenderer::setSprite(csSpriteRef sprite) {
+    dat->sprite = sprite;
+    
+    if (cq_flt_equal(dat->size.x, 0) || cq_flt_equal(dat->size.y, 0)) {
+        csTexture2DRef texture;
+        if (sprite != nullptr) {
+            texture = sprite->texture();
+        }
+        if (texture != nullptr) {
+            dat->size.x = texture->width () / cq_wnd_scale();
+            dat->size.y = texture->height() / cq_wnd_scale();
+        }
+    }
+}
+
+csSpriteRef csSpriteRenderer::sprite() {
+    return dat->sprite;
+}
+
+void csSpriteRenderer::setSize(csVector2 size) {
+    dat->size = size;
+}
+
+csVector2 csSpriteRenderer::size() {
+    return dat->size;
 }
--- program_game/gamekit/libgamekit/cppgamekit/csspriterenderder.hh
+++ program_game/gamekit/libgamekit/cppgamekit/csspriterenderder.hh
@@ -1,12 +1,20 @@
 #pragma once
 
+#include "csgeometry.hh"
 #include "csrenderer.hh"
+#include "cssprite.hh"
 
 _CSGAMEKIT_BEGIN_VERSION_NS
 
 cq_class(csSpriteRenderer, csRenderer) {
     
     void handleRender() override;
+    
+    virtual void setSprite(csSpriteRef sprite);
+    virtual csSpriteRef sprite();
+    
+    virtual void setSize(csVector2 size);
+    virtual csVector2 size();
 };
 
 _CSGAMEKIT_END_VERSION_NS
