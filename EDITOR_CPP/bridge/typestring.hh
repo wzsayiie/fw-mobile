47d1a13f0bafd7d8702e91d39eac9f92634646d8
wzsayiie <wzsayiie@qq.com>
2020-02-02 18:34:30 +0800

    add typestring.hh

---
+++ EDITOR_CPP/bridge/typestring.hh
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "metapicker.hh"
+
+string cpp_type_str  (const meta_info &meta, _type type);
+string cpp_type_str_p(const meta_info &meta, _type type);
+string objc_type_str (const meta_info &meta, _type type);
