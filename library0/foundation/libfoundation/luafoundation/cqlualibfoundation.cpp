43f62a65b6701fb66e74036f06ef48a5f5491d26
wzsayiie <wzsayiie@qq.com>
2020-02-03 14:47:33 +0800

    libfoundation add interfaces for lua.

--- library0/foundation/libfoundation/luafoundation/cqlualibfoundation.cpp
+++ library0/foundation/libfoundation/luafoundation/cqlualibfoundation.cpp
@@ -1,6 +1,42 @@
 #include "cqlualibfoundation.h"
 #include "cqfoundationarche.h"
 
+//host os:
+
+static int32_t cq_host_os(lua_State *state) {
+    
+    CQ_IF_ON_ANDROID(return cq_lua_return_string(state, "android"));
+    CQ_IF_ON_IOS    (return cq_lua_return_string(state, "ios"    ));
+    CQ_IF_ON_WINDOWS(return cq_lua_return_string(state, "windows"));
+    CQ_IF_ON_OSX    (return cq_lua_return_string(state, "osx"    ));
+}
+
+//app bundle resource:
+
+static int32_t cq_ios_bundle_path(lua_State *state) {
+    const char *path = cq_ios_bundle_path();
+    
+    return cq_lua_return_string(state, path);
+}
+
+static int32_t cq_ios_bundle_res_path(lua_State *state) {
+    const char *name = cq_lua_check_string(state, 1);
+    const char *type = cq_lua_check_string(state, 2);
+    
+    const char *path = cq_ios_bundle_res_path(name, type);
+    
+    return cq_lua_return_string(state, path);
+}
+
+static int32_t cq_andr_copy_asset(lua_State *state) {
+    const char *from = cq_lua_check_string(state, 1);
+    const char *to   = cq_lua_check_string(state, 2);
+    
+    bool success = cq_andr_copy_asset(from, to);
+    
+    return cq_lua_return_bool(state, success);
+}
+
 //log:
 
 static int32_t cq_log_info(lua_State *state) {
@@ -70,14 +106,47 @@ static int32_t cq_remove_path(lua_State *state) {
     return cq_lua_return_void(state);
 }
 
+static int32_t cq_open_dir(lua_State *state) {
+    const char *path = cq_lua_check_string(state, 1);
+    
+    bool sucess = cq_open_dir(path);
+    
+    return cq_lua_return_bool(state, sucess);
+}
+
+static int32_t cq_read_dir(lua_State *state) {
+    const char *item = cq_read_dir();
+    
+    return cq_lua_return_string(state, item);
+}
+
+static int32_t cq_close_dir(lua_State *state) {
+    cq_close_dir();
+    
+    return cq_lua_return_void(state);
+}
+
 //register
 
 void cq_lua_load_lib_foundation() {
     
 #define register_func(name) cq_lua_register_func(#name, name)
     
+    //host os:
+    register_func(cq_host_os);
+    
+    //log:
+    
     register_func(cq_log_info );
     register_func(cq_log_error);
+    
+    //app bundle resource:
+
+    register_func(cq_ios_bundle_path    );
+    register_func(cq_ios_bundle_res_path);
+    register_func(cq_andr_copy_asset    );
+    
+    //file access:
     
     register_func(cq_document_directory );
     register_func(cq_caches_directory   );
@@ -86,4 +155,8 @@ void cq_lua_load_lib_foundation() {
     register_func(cq_file_exists        );
     register_func(cq_create_directory   );
     register_func(cq_remove_path        );
+    
+    register_func(cq_open_dir );
+    register_func(cq_read_dir );
+    register_func(cq_close_dir);
 }
