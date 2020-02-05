d00f7198e2f696343234075ae4311550864956f4
wzsayiie <wzsayiie@qq.com>
2020-02-05 17:03:30 +0800

    libfoundation add founction that post task to system main run loop.

--- library0/ctool/libctool/archetype/cqctoolarche.h
+++ library0/ctool/libctool/archetype/cqctoolarche.h
@@ -2,5 +2,6 @@
 
 #include "cqcbasis.h"
 
+CQ_C_LINK void cq_main_queue_user_control(bool enabled);
 CQ_C_LINK bool cq_main_queue_empty(void);
 CQ_C_LINK void cq_update_main_queue(void);
--- library0/ctool/libctool/cppctool/CQCTOOL_PORT.cpp
+++ library0/ctool/libctool/cppctool/CQCTOOL_PORT.cpp
@@ -1,6 +1,10 @@
 #include "cqctoolarche.h"
 #include "cqctool.hh"
 
+void cq_main_queue_user_control(bool enabled) {
+    cqDispatch::setUserControlEnabled(enabled);
+}
+
 bool cq_main_queue_empty() {
     return cqDispatch::mainQueueEmpty();
 }
