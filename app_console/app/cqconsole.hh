786877602937972d6c7f1c95273505aa868758d4
wzsayiie <wzsayiie@qq.com>
2020-10-04 21:20:00 +0800

    remove app_console/ .

--- app_console/app/cqconsole.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "cqfoundation.hh"
-#include "cqctool.hh"
-
-void launch(int argc, const char *argv[]);
--- app_console/app/main.cpp
+++
@@ -1,4 +0,0 @@
-#include "cqconsole.hh"
-
-void launch(int argc, const char *argv) {
-}
