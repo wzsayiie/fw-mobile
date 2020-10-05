dce0626dad75c94cc89d0000f70b30b43dfa9682
wzsayiie <wzsayiie@qq.com>
2020-10-05 17:30:51 +0800

    removed the dependence of app_ios on xapp and xgame.

--- app_ios/App/main.m
+++ app_ios/App/main.m
@@ -1,10 +1,3 @@
 int main(int argc, char *argv[]) {
-    
-    NSString *delegate = nil;
-    
-    delegate = @"AppDelegate";
-//    delegate = @"AppAppDelegate";
-//    delegate = @"GameAppDelegate";
-  
-    return UIApplicationMain(argc, argv, nil, delegate);
+    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
 }
